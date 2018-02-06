f2e6b61a41374b8344dc8bbba898d8eeab9fef91
SteveLeong <Steven@Stevens-MBP.hitronhub.home>
2018-02-05 17:03:14 -0700

    words3

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 function getStats(txt) {
     return {
         nChars: txt.length,
-        nWords: txt.split(/\W+/).length, //Split at all non-word characters
+        nWords: txt.split(/\W+/).filter(function(e){return e}).length, //Split at all non-word characters (including repeating non-word characters)
         nLines: 10,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
